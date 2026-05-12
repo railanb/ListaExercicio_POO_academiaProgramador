d4c5a53d03454450ceb75b026cc73df3fa65309c
railan <railanb.rb@hotmail.com>
2026-05-12 13:46:04 -0300

    enviando o projeto incial

---
+++ Exercicio01.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("╔══════════════╗");
+Console.WriteLine("║   SISTEMA    ║");
+Console.WriteLine("╚══════════════╝");
+
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("╔══════════════╗");
+Console.WriteLine("║   SISTEMA    ║");
+Console.WriteLine("╚══════════════╝");
+
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("╔══════════════╗");
+Console.WriteLine("║   SISTEMA    ║");
+Console.WriteLine("╚══════════════╝");
+
+Console.ResetColor();
+Console.ReadLine();
